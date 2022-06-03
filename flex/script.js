d50c7d47b40b3e031d31a0f3ffc88453d77bfe58
Johannes Maendle <johannes@studysmarter.de>
2022-06-03 13:52:04 +0200

    add pointer event example

---
+++ flex/script.js
@@ -0,0 +1,36 @@
+const video = document.querySelector(".video");
+
+video.addEventListener("pointerdown", (e) => {
+  const dot = document.createElement("div");
+  dot.classList.add("dot");
+  dot.id = e.pointerId;
+  positionDot(e, dot);
+  video.append(dot);
+});
+
+video.addEventListener("pointermove", (e) => {
+  const dot = document.getElementById(e.pointerId);
+  if (dot === null) return;
+  positionDot(e, dot);
+});
+
+video.addEventListener("pointercancel", (e) => {
+  clearPointer(e);
+});
+
+video.addEventListener("pointerup", (e) => {
+  clearPointer(e);
+});
+
+function positionDot(e, dot) {
+  dot.style.width = e.width * 10 + "px";
+  dot.style.height = e.height * 10 + "px";
+  dot.style.left = e.pageX - 15 + "px";
+  dot.style.top = e.pageY - 15 + "px";
+}
+
+function clearPointer(e) {
+  const dot = document.getElementById(e.pointerId);
+  if (dot === null) return;
+  dot.remove();
+}
